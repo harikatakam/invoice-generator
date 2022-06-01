d38209980648fef7449e2c65c6245dcf0be42445
Fazil <mohammed.fazil@simplifyvms.com>
2022-06-01 21:09:07 +0530

    intial changes

---
+++ index.js
@@ -0,0 +1,19 @@
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
+
+// express()
+//   .use('/static', express.static(path.join(__dirname, 'public')))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
