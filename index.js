858aca5ddbc922c0353155c2d118bdeafaef1263
Fazil <mohammed.fazil@simplifyvms.com>
2022-06-01 21:26:57 +0530

    root changes

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
